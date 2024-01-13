011d28b443c9cb82cfb8bb22990c6c9b3731cf00
Fanny Sarrazin <fanny.sarrazin@ufz.de>
2024-01-13 21:05:26 +0100

    correct typo in header

--- src/model_NP_emissions/model_NP_emissions.py
+++ src/model_NP_emissions/model_NP_emissions.py
@@ -9,7 +9,7 @@ LICENSE:
     All rights reserved.
     
     This source code is licensed under the GNU General Public License v3.0 
-    license  found in the LICENSE file in the root directory of this source tree. 
+    found in the LICENSE file in the root directory of this source tree. 
 
 
 PLEASE CITE THIS PUBLICATION IF YOU USE THIS CODE:
